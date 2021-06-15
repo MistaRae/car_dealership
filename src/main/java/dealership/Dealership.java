e84ecd764e942e1279ea29ef2a54a6883ef01b84
MistaRae <itsamistarae@gmail.com>
2021-06-15 18:09:09 +0100

    dealership constructor built

--- src/main/java/dealership/Dealership.java
+++ src/main/java/dealership/Dealership.java
@@ -1,4 +1,24 @@
 package dealership;
 
+import Car.Car;
+
+import java.util.ArrayList;
+
 public class Dealership {
+
+    private ArrayList<Car> stock;
+    private int till;
+
+    public Dealership(ArrayList stock, int till) {
+        this.stock = stock;
+        this.till = till;
+    }
+
+    public ArrayList getStock() {
+        return stock;
+    }
+
+    public int getTill() {
+        return till;
+    }
 }
